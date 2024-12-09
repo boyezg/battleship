0e902506fd71d6c7361dd6c883fbd5e6a05def3d
boyezg <boyezgollapalli@gmail.com>
2024-12-09 22:43:11 +0530

    update code

---
+++ public/guide.js
@@ -0,0 +1,9 @@
+// Function to show the dialog box
+function showDialog() {
+    document.getElementById('dialog').style.display = 'flex';
+}
+
+// Function to close the dialog box
+function closeDialog() {
+    document.getElementById('dialog').style.display = 'none';
+}
